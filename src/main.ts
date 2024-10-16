fe89271f7857e31cbffd7631f676431fed96d1a1
Jesussalman <jesussalman@gmail.com>
2024-10-16 16:42:55 -0400

    Taller 1

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as morgan from 'morgan';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
+  const logger = new Logger('Main');
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(process.env.PORT);
+  logger.log(`Server running on port: ${process.env.PORT}`);
+  logger.log(`Server running on url: ${await app.getUrl()}`);
+}
+bootstrap();
